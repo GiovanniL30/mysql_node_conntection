8575bdabd46b212c43a787605a66f4cf4823b8b8
Giovanni Leo <giovannileo100@gmail.com>
2024-09-03 19:12:42 +0800

    finished the frontend of register

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import authRoute from "./routes/auth.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/", authRoute);
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,135 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const Register = () => {
+  const [data, setData] = useState({
+    firstname: "",
+    lastname: "",
+    username: "",
+    password: "",
+  });
+  const [error, setError] = useState(false);
+
+  function onChange(e) {
+    const { value, name } = e.target;
+    setData((prev) => ({ ...prev, [name]: value }));
+  }
+
+  async function register() {
+    try {
+      const response = await fetch("http://localhost:1099/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          firstName: data.firstname,
+          lastName: data.lastname,
+          password: data.password,
+          userName: data.username,
+        }),
+      });
+
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log(responseData);
+      } else {
+        const errorData = await response.json();
+        setError(`Error: ${errorData.message || "Registration failed"}`);
+      }
+    } catch (err) {
+      setError(`Network Error: ${err.message}`);
+    }
+  }
+
   return (
-    <div className="padding">
+    <div className="padding-x">
       <div className="max-container">
-        <h1>Register</h1>
+        <div className="flex flex-col items-center h-screen justify-center">
+          <h1 className="text-center font-bold text-3xl">Register</h1>
+          <form className="flex flex-col gap-y-5 max-w-[600px] w-full mt-5">
+            <div className="floating-label-wrapper">
+              <input
+                name="firstname"
+                type="text"
+                value={data.firstname}
+                placeholder=" "
+                onChange={(e) => onChange(e)}
+                className="border border-gray-300 rounded-md p-2 w-full outline-blue-200"
+              />
+              <label
+                htmlFor="firstname"
+                className="floating-label text-gray-500 "
+              >
+                First Name
+              </label>
+            </div>
+            <div className="floating-label-wrapper">
+              <input
+                name="lastname"
+                type="text"
+                placeholder=" "
+                value={data.lastname}
+                onChange={(e) => onChange(e)}
+                className="border border-gray-300 rounded-md p-2 w-full outline-blue-200"
+              />
+              <label
+                htmlFor="lastname"
+                className="floating-label text-gray-500 "
+              >
+                Last Name
+              </label>
+            </div>
+            <div className="floating-label-wrapper">
+              <input
+                name="username"
+                type="text"
+                placeholder=" "
+                value={data.username}
+                onChange={(e) => onChange(e)}
+                className="border border-gray-300 rounded-md p-2 w-full outline-blue-200"
+              />
+              <label
+                htmlFor="firstname"
+                className="floating-label text-gray-500 "
+              >
+                User Name
+              </label>
+            </div>
+            <div className="floating-label-wrapper">
+              <input
+                name="password"
+                type="password"
+                placeholder=" "
+                value={data.password}
+                onChange={(e) => onChange(e)}
+                className="border border-gray-300 rounded-md p-2 w-full outline-blue-200"
+              />
+              <label
+                htmlFor="firstname"
+                className="floating-label text-gray-500 "
+              >
+                Password
+              </label>
+            </div>
+            <button
+              type="button"
+              onClick={register}
+              className="w-full bg-blue-400 text-white py-2 rounded-md hover:opacity-50 duration-500 ease-linear "
+            >
+              Submit
+            </button>
+          </form>
+          <div className="flex gap-2 text-gray-700 mt-6">
+            <p>Already have an account? </p>
+            <NavLink
+              to="/login"
+              className="underline hover:opacity-5 duration-500 ease-linear "
+            >
+              Login
+            </NavLink>
+          </div>
+        </div>
       </div>
     </div>
   );
